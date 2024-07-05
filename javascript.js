f60df19868fa56aff89e439f1f9a47823820e44e
Osikoya Michael <88806689+osikoyamichael@users.noreply.github.com>
2024-07-05 20:52:22 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,123 @@
+function result(){
+
+    var score=0;
+    var quiz=document.getElementById("quiz");
+    var result=document.getElementById('result');
+   if (document.getElementById('correct1').checked){
+        score++;
+    }
+
+    if (document.getElementById('correct2').checked){
+        score++;
+    }
+    if (document.getElementById('correct3').checked){
+        score++;
+    
+      }
+      if (document.getElementById('correct4').checked){
+        score++;
+    }
+    if (document.getElementById('correct5').checked){
+        score++;
+    }
+    if (document.getElementById('correct6').checked){
+        score++;
+    }
+    if (document.getElementById('correct7').checked){
+        score++;
+    }
+    if (document.getElementById('correct8').checked){
+        score++;}
+        if (document.getElementById('correct9').checked){
+            score++;}
+    if (document.getElementById('correct10').checked){
+        score++;}
+        if (document.getElementById('correct11').checked){
+            score++;}
+            if (document.getElementById('correct12').checked){
+                score++;}
+                if (document.getElementById('correct13').checked){
+                    score++;
+                }
+
+                if (document.getElementById('correct14').checked){
+                    score++;
+                }
+                if (document.getElementById('correct15').checked){
+                    score++;
+                }
+                if (document.getElementById('correct16').checked){
+                    score++;
+                }
+
+                if (document.getElementById('correct17').checked){
+                    score++;
+                }if (document.getElementById('correct18').checked){
+                    score++;
+                }if (document.getElementById('correct19').checked){
+                    score++;
+                }if (document.getElementById('correct20').checked){
+                    score++;
+                }
+                if (document.getElementById('correct21').checked){
+                    score++;
+                }
+                if (document.getElementById('correct22').checked){
+                    score++;
+                }if (document.getElementById('correct23').checked){
+                    score++;
+                }if (document.getElementById('correct24').checked){
+                    score++;
+                }if (document.getElementById('correct25').checked){
+                    score++;
+                }if (document.getElementById('correct26').checked){
+                    score++;
+                }if (document.getElementById('correct27').checked){
+                    score++;
+                }if (document.getElementById('correct28').checked){
+                    score++;
+                }
+                if (document.getElementById('correct29').checked){
+                    score++;
+                }
+                if (document.getElementById('correct30').checked){
+                    score++;
+                }
+                if (document.getElementById('correct31').checked){
+                    score++;
+                }if (document.getElementById('correct32').checked){
+                    score++;
+                }if (document.getElementById('correct33').checked){
+                    score++;
+                }if (document.getElementById('correct34').checked){
+                    score++;
+                }if (document.getElementById('correct35').checked){
+                    score++;
+                }if (document.getElementById('correct36').checked){
+                    score++;
+                }if (document.getElementById('correct37').checked){
+                    score++;
+                }if (document.getElementById('correct38').checked){
+                    score++;
+                }
+                if (document.getElementById('correct39').checked){
+                    score++;
+                }if (document.getElementById('correct40').checked){
+                    score++;
+                }
+
+
+
+    alert( "OUT OF 40 YOU SCORE:"+score);
+
+
+
+
+
+    
+    
+
+
+  }
+    
+  
